b64503fc8aadbb7001966cbcdacee700acf0dd80
oliverpauffley <mrpauffley@gmail.com>
2024-01-30 11:03:33 +0000

    update dict

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import           Control.Monad (forever)
+import           Data.Char     (toLower)
+import           Data.List     (intersperse)
+import           Data.Maybe    (isJust)
+import           System.Exit   (exitSuccess)
+import           System.IO     (BufferMode (NoBuffering), hSetBuffering, stdout)
+import           System.Random (randomRIO)
+
 main :: IO ()
 main = putStrLn "Hello, World!"
